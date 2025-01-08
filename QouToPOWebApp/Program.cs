4085e6e016bf8d077825753c478a02fad2546338
Vlad <v.redula@faradaygroup.com>
2025-01-08 16:12:52 +0900

    Added pdfiumViewer as service

--- QouToPOWebApp/Program.cs
+++ QouToPOWebApp/Program.cs
@@ -1,7 +1,10 @@
+using QouToPOWebApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IPdfViewerService, PdfiumViewerService>();
 
 var app = builder.Build();
 
---
+++ QouToPOWebApp/Services/PdfiumViewerService.cs
@@ -0,0 +1,36 @@
+﻿using PdfiumViewer;
+using System.Drawing.Imaging;
+
+namespace QouToPOWebApp.Services
+{
+
+    public interface IPdfViewerService
+    {
+        List<byte[]> GenerateThumbnails(string filePath);
+    }
+
+    public class PdfiumViewerService : IPdfViewerService
+    {
+        public List<byte[]> GenerateThumbnails(string filePath)
+        {
+            var thumbnails = new List<byte[]>();
+
+            using (var pdfDocument = PdfDocument.Load(filePath))
+            {
+                for (int i = 0; i < pdfDocument.PageCount; i++)
+                {
+                    using (var image = pdfDocument.Render(i, 200, 200, true))
+                    {
+                        using (var ms = new MemoryStream())
+                        {
+                            image.Save(ms, ImageFormat.Png);
+                            thumbnails.Add(ms.ToArray());
+                        }
+                    }
+                }
+            }
+
+            return thumbnails;
+        }
+    }
+}
