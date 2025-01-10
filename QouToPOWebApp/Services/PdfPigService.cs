4bad0b99ca63e38db2221dd88aa44f50bd504db8
Vlad <v.redula@faradaygroup.com>
2025-01-10 15:30:31 +0900

    Added pdf pig and tabula library

---
+++ QouToPOWebApp/Services/PdfPigService.cs
@@ -0,0 +1,22 @@
+﻿using UglyToad.PdfPig;
+
+namespace QouToPOWebApp.Services
+{
+    public class PdfPigService
+    {
+        public List<string> ExtractText(string pdfPath)
+        {
+            var extractedText = new List<string>();
+
+            using (var document = PdfDocument.Open(pdfPath))
+            {
+                foreach (var page in document.GetPages())
+                {
+                    extractedText.Add(page.Text);
+                }
+            }
+
+            return extractedText;
+        }
+    }
+}
---
+++ QouToPOWebApp/Services/TabulaService.cs
@@ -0,0 +1,29 @@
+﻿using Tabula;
+using Tabula.Detectors;
+using Tabula.Extractors;
+using UglyToad.PdfPig;
+
+namespace QouToPOWebApp.Services
+{
+    public class TabulaService
+    {
+        public void ExtractTables(string pdfPath)
+        {
+            var extractedTables = new List<List<string>>();
+
+            using (var stream = new FileStream(pdfPath, FileMode.Open, FileAccess.Read))
+            {
+                var pdoc = PdfDocument.Open(stream, new ParsingOptions { SkipMissingFonts = true, UseLenientParsing = true });
+                PageArea area = ObjectExtractor.ExtractPage(pdoc, 1);
+
+                SimpleNurminenDetectionAlgorithm da = new SimpleNurminenDetectionAlgorithm();
+                var sample = da.Detect(area);
+
+                IExtractionAlgorithm ea = new BasicExtractionAlgorithm();
+                List<Table> tables = (List<Table>)ea.Extract(area.GetArea(sample[0].BoundingBox)); // take first candidate area
+                var table = tables[0];
+                var rows = table.Rows;
+            }
+        }
+    }
+}
