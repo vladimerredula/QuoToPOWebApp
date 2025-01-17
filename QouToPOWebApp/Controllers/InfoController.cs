bc4fe5986c7f6e7d3688d376a8d9ea9d5b041606
Vlad <v.redula@faradaygroup.com>
2025-01-17 17:21:33 +0900

    Added Info controller

---
+++ QouToPOWebApp/Controllers/InfoController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QouToPOWebApp.Models;
+
+namespace QouToPOWebApp.Controllers
+{
+    public class InfoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InfoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #region Company Functions
+        // GET: Company
+        public async Task<IActionResult> Company()
+        {
+            return View(await _context.Companies.ToListAsync());
+        }
+
+        // GET: Info/DetailsCompany/5
+        public async Task<IActionResult> DetailsCompany(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .FirstOrDefaultAsync(m => m.Company_ID == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_CompanyDisplayPartial", company);
+        }
+
+        // GET: Info/Create
+        public IActionResult CreateCompany()
+        {
+            return PartialView("_CompanyFormPartial");
+        }
+
+        // POST: Info/CreateCompany
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCompany([Bind("Company_name,Company_name_jpn,Address,Address_jpn,Postal_code,Telephone,Fax")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(company);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(nameof(Company), company);
+        }
+
+        // GET: Info/EditCompany/5
+        public async Task<IActionResult> EditCompany(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_CompanyFormPartial", company);
+        }
+
+        // POST: Info/EditCompany/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditCompany(int id, [Bind("Company_ID,Company_name,Company_name_jpn,Address,Address_jpn,Postal_code,Telephone,Fax")] Company company)
+        {
+            if (id != company.Company_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(company);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompanyExists(company.Company_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Company));
+            }
+            return View(nameof(Company), company);
+        }
+
+        // GET: Info/DeleteCompany/5
+        public async Task<IActionResult> DeleteCompany(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .FirstOrDefaultAsync(m => m.Company_ID == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_CompanyDisplayPartial", company);
+        }
+
+        // POST: Info/DeleteCompany/5
+        [HttpPost, ActionName("DeleteCompany")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteCompanyConfirmed(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+            if (company != null)
+            {
+                _context.Companies.Remove(company);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Company));
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Companies.Any(e => e.Company_ID == id);
+        }
+        #endregion
+    }
+}
