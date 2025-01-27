2ef8ff6729f088c004622b376007c3b723dbc26d
Vlad <v.redula@faradaygroup.com>
2025-01-27 17:51:33 +0900

    Added Po and Quotation view model

---
+++ QouToPOWebApp/ViewModel/PoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QouToPOWebApp.Models;
+
+namespace QouToPOWebApp.ViewModel
+{
+    [Keyless]
+    public class PoViewModel
+    {
+        public Po Po { get; set; }
+        public List<Quotation_item>? Quotation_items { get; set; }
+    }
+}
---
+++ QouToPOWebApp/ViewModel/QuotationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QouToPOWebApp.Models;
+
+namespace QouToPOWebApp.ViewModel
+{
+    [Keyless]
+    public class QuotationViewModel
+    {
+        public Quotation Quotation { get; set; }
+        public List<Quotation_item>? Items { get; set; }
+    }
+}
