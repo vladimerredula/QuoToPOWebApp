6e0799a0f6b1e4db3116d812c0dd08e8d0b8be15
Vlad <v.redula@faradaygroup.com>
2025-01-30 15:42:39 +0900

    Added login function

--- QouToPOWebApp/Program.cs
+++ QouToPOWebApp/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using QouToPOWebApp;
 using QouToPOWebApp.Services;
@@ -18,6 +19,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
         new MySqlServerVersion(new Version(8, 0, 32)) // Adjust MySQL version
     ));
 
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Access/Login";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+        //options.AccessDeniedPath = "/AccessDenied";
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true; // Ensure session cookie is not accessible to client-side scripts
+    options.Cookie.IsEssential = true; // Indicate that the session cookie is essential for the application
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // Send session cookie only over HTTPS
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Session timeout set to 30 minutes
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,8 +54,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseAuthentication();
+
+app.UseSession();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    //pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Access}/{action=Index}/{id?}");
 
 app.Run();
