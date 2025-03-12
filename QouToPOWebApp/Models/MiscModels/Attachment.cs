f4b731e93c446be2ee7f32320ebb176bd95b8570
Vlad <v.redula@faradaygroup.com>
2025-03-12 17:53:31 +0900

    Added file group and attachment model

---
+++ QouToPOWebApp/Models/MiscModels/Attachment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QouToPOWebApp.Models.MiscModels
+{
+    public class Attachment
+    {
+        [Key]
+        public int Attachment_ID { get; set; }
+        public int User_ID { get; set; }
+        public string File_name { get; set; }
+        public string File_path { get; set; }
+        public string Attachment_type { get; set; }
+        public int File_group_ID { get; set; }
+        public DateTime Date_uploaded { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ QouToPOWebApp/Models/MiscModels/File_group.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QouToPOWebApp.Models.MiscModels
+{
+    public class File_group
+    {
+        [Key]
+        public int File_group_ID { get; set; }
+        public DateTime Date_created { get; set; }
+        public string Directory_path { get; set; }
+        public int User_ID { get; set; }
+
+    }
+}
